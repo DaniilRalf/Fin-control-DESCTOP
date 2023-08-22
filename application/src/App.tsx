ab5dfed3ec0e955b2d9a8e611b14bf503cdbcc1b
DaniilRalf <ralf.danya@mail.ru>
2023-08-23 05:21:30 +0700

    react_config

--- application/src/App.tsx
+++ application/src/App.tsx
@@ -1,41 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.scss';
+import React from 'react'
+import './App.scss'
 
 const electron = window.require('electron');
 const ipcRenderer  = electron.ipcRenderer;
 
 function App() {
 
-  console.log('===========================')
-  console.log(ipcRenderer)
-  console.log('===========================')
+  // console.log('===========================')
+  // console.log(ipcRenderer)
+  // console.log('===========================')
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          as
-          dfasdfasdfaasdfa
-          sdfasdf
-          asd
-          fa
-          sd
-          fas
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      app-start
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
